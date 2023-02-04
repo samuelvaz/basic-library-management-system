b2753a9dd900dc7ef8a6b28f260405d068297762
samuelvaz <54169944+samuelvaz@users.noreply.github.com>
2023-02-04 14:49:44 +0530

    Add files via upload

---
+++ src/config/config.php
@@ -0,0 +1,9 @@
+<?php
+ob_start(); // turns on output buffering
+session_start();
+$timezone = date_default_timezone_set("Asia/Kolkata");
+$con = mysqli_connect("localhost","root","","library" ); // conection variable
+if(mysqli_connect_errno()){
+	echo "Failed to connect" .mysqli_connect_errno();
+}
+?>
